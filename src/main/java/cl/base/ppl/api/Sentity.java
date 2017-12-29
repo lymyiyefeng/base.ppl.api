dbb4c922f580b72b552887fc1d5c78c38a8d501e
lymyiyefeng <lymyiyefeng@163.com>
2017-12-30 00:45:26 +0800

    ppl.api
    分词实体

---
+++ src/main/java/cl/base/ppl/api/Sentity.java
@@ -0,0 +1,12 @@
+package cl.base.ppl.api;
+
+import java.io.Serializable;
+
+public class Sentity implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1355089571670376004L;
+
+}
